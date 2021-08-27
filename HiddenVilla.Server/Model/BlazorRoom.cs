77f4af6767e7028c06d86ebc3726d3a4b0ae68ba
tysongibby <tyson.gibby@outlook.com>
2021-08-27 11:10:42 -0600

    12. One way data binding

---
+++ HiddenVilla.Server/Model/BlazorRoom.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiddenVilla.Server.Model
+{
+    public class BlazorRoom
+    {
+        public int Id { get; set; }
+        public string RoomName { get; set; }
+        public double Price { get; set; }
+        public bool IsActive { get; set; }
+
+    }
+}
