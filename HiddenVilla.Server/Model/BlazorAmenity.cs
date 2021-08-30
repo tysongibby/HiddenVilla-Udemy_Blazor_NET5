41da45a5aeabe33fea42bbf88a93357af5b0e331
tysongibby <tyson.gibby@outlook.com>
2021-08-30 10:56:58 -0600

    23 - 27

---
+++ HiddenVilla.Server/Model/BlazorAmenity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiddenVilla.Server.Model
+{
+    public class BlazorAmenity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}
