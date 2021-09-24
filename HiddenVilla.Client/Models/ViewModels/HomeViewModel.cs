d03feb6fe2f08717d5ea3bad24f0450e6e0d0fbc
Tyson Gibby <tyson.gibby@outlook.com>
2021-09-24 16:39:03 -0600

    130 - 136

---
+++ HiddenVilla.Client/Models/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiddenVilla.Client.Models.ViewModels
+{
+    public class HomeViewModel
+    {
+        public DateTime StartDate { get; set; } = DateTime.Now;
+        public DateTime EndDate { get; set; }
+        public int NumberOfNights { get; set; }
+    }
+}
